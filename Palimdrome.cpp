c25879b650ae7aec2a5fce68b97bf3311ddcd25d
aashik <ak8395860@gmail.com>
2026-03-18 10:26:11 +0530

    Update Palimdrome.cpp

--- Palimdrome.cpp
+++ Palimdrome.cpp
@@ -1,3 +1,35 @@
+#include<iostream>
+using namespace std;
 
-hello
-jskeiei
+bool AlphaNum(char ch){
+    if((ch>='a' && ch<='z') || (ch >='0' && ch<='9') ){
+        return true;}
+    return false;
+    }
+
+bool ispalindrome(string str){
+    
+    int st = 0,end = str.length()-1;
+    while(st<end){
+        if(!AlphaNum(str[st])){
+          st++;
+          continue;
+        }
+        if(!AlphaNum(str[end])){
+            end--;
+            continue;
+        }
+           if(str[st]!=str[end]){
+        return false;
+    }
+    st++,end--;
+    return true;
+    }
+   
+}
+
+int main(){
+    cout<<ispalindrome("#27bsue")<<endl;
+}
+
+
